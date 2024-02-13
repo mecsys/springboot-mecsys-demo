c7244e1be3c7d6edd8e4da120a8ef5ccf55d385c
Isaac Mecchi <isaac.mecchi@sensedia.com>
2024-02-13 13:56:45 -0300

    Initial commit.

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import io.micrometer.common.KeyValue;
+import io.micrometer.common.KeyValues;
+import io.micrometer.observation.ObservationFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("test")
+	public String testEndpoint() {
+		return "Hello world";
+	}
+
+	@Bean
+	public ObservationFilter customTagFilter() {
+		return context -> {
+			log.info("Executing observation filter");
+
+			if (context instanceof ServerRequestObservationContext observationContext) {
+				KeyValues keyValues = KeyValues.empty();
+
+				if (
+						observationContext.getCarrier() != null &
+						observationContext.getCarrier().getHeader("customerId") != null &
+						observationContext.getCarrier().getHeader("environmentId") != null
+				) {
+					var customerId = observationContext.getCarrier().getHeader("customerId");
+					var environmentId = observationContext.getCarrier().getHeader("environmentId");
+
+					keyValues = keyValues
+							.and(KeyValue.of("customerId", customerId))
+							.and(KeyValue.of("environmentId", environmentId));
+				}
+
+				context.addLowCardinalityKeyValues(keyValues);
+			}
+			return context;
+		};
+	}
+}
