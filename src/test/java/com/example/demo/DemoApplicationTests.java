53170b36019c280b0f1dced14a9b4c01abe684b7
Isaac Mecchi <isaac.mecchi@sensedia.com>
2026-02-10 12:18:15 -0300

    Add new libs for micrometer.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -11,3 +11,28 @@ class DemoApplicationTests {
 	}
 
 }
+
+@RestController
+class TestController {
+
+    private final WebClient webClient;
+
+    TestController(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("https://echo.mecsys.com.br").build();
+    }
+
+    @GetMapping("/test")
+    public Mono<String> test() {
+        return webClient.get()
+                .uri("/downstream")
+                .retrieve()
+                .bodyToMono(String.class)
+                .thenReturn("ok");
+    }
+
+    @GetMapping("/downstream")
+    public String downstream() {
+        return "downstream-ok";
+    }
+}
+
